8fe0131edf86f4bae6e94d3fdf5392b15f1086f6
Ray888Ray <ilimirgashev@gmail.com>
2022-11-12 19:32:54 +0600

    Changed fixtures

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,3 +5,6 @@ class BookGuestForm(forms.Form):
     name = forms.CharField(max_length=20, required=True, label='name')
     content = forms.CharField(max_length=300, required=True, label='content')
     email = forms.EmailField(required=True, label='email')
+
+
+
